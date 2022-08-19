854a60cc346fce9fe644fcdce19939978f17d2a9
lucassouzapb <lycasoliveira@gmail.com>
2022-08-19 11:28:42 -0300

    Criando home

--- lib/view/quiz/quiz.dart
+++ lib/view/quiz/quiz.dart
@@ -5,6 +5,11 @@ class QuizView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+      ),
+      body: Stack(),
+    );
   }
 }
